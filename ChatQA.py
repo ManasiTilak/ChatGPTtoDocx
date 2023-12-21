ecb5aa6661e0a035e6df2029ad60bd121790c1b2
unknown <manasitilak12@gmail.com>
2023-12-21 17:08:34 +0530

    Saves Output to a .docx file

--- ChatQA.py
+++ ChatQA.py
@@ -2,22 +2,52 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import openai
 import os
+from docx import Document
 
-load_dotenv()
-
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
-try:
-    # Create a chat completion
+def get_openai_response(client, model, question):
+    """
+    Function to get a response from the OpenAI API.
+    """
+    # print(model)
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model=model,
         messages=[
             {"role": "system", "content": "You are a helpful assistant and a coding expert. You are very good at writing coding blogs."},
-            {"role": "user", "content": "Capital of India?"}
-        ]
+            {"role": "user", "content": question}
+        ],
+        max_tokens=10,
     )
-    
-    # Print the response content
-    print(response.choices[0].message.content)
-except Exception as e:
-    print(f"An error occurred: {e}")
+    return response.choices[0].message.content
+
+def create_docx(content, filename):
+    """
+    Function to create a .docx file from the provided content.
+    """
+    doc = Document()
+    doc.add_paragraph(content)
+    doc.save(filename)
+    print(f"Response saved in '{filename}'")
+
+def main():
+    """
+    Main function to orchestrate the OpenAI API call and document creation.
+    """
+
+    # Load environment variables
+    load_dotenv()
+    # Initialize the OpenAI client with API key
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    # Specify model
+    model="gpt-3.5-turbo"
+    # Specify Question
+    question="Capital of India?"
+    # Specify Filename for output File
+    filename="response.docx"
+    try:
+        response_content = get_openai_response(client,model,question)
+        create_docx(response_content, filename)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
