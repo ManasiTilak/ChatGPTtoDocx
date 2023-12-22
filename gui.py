99a075180eb2094ea5030c84abedd8884f6367a8
unknown <manasitilak12@gmail.com>
2023-12-22 11:32:31 +0530

    Improved UI

--- gui.py
+++ gui.py
@@ -6,7 +6,8 @@ class AppWindow(QMainWindow):
     def __init__(self, submit_callback, save_callback):
         super().__init__()
         self.setWindowTitle("PyQt Interface - OpenAI Chat")
-        self.setGeometry(100, 100, 600, 350)
+        # self.setGeometry(100, 100, 600, 350)
+        self.setGeometry(100, 100, 800, 600)
 
         # Store the submit_callback function
         self.submit_callback = submit_callback
@@ -14,18 +15,24 @@ class AppWindow(QMainWindow):
         
         # Create layout
         layout = QVBoxLayout()
+        layout.setContentsMargins(10, 10, 10, 10)  # Add some margins around the layout
+        layout.setSpacing(10)  # Add spacing between widgets
 
         # Create widgets for input and submit
         self.input_line = QLineEdit(self)
         self.submit_button = QPushButton('Submit', self)
         self.submit_button.clicked.connect(self.on_submit_click)
 
+        # Add a Save to .docx button
+        self.save_button = QPushButton('Save to .docx', self)
+        self.save_button.clicked.connect(self.on_save_click)
+
         # Create widgets and layout for max_tokens
         self.max_tokens_checkbox = QCheckBox("Specify Max Tokens", self)
         self.max_tokens_checkbox.stateChanged.connect(self.max_tokens_checkbox_changed)
         self.max_tokens_spin = QSpinBox(self)
         self.max_tokens_spin.setMinimum(1)
-        self.max_tokens_spin.setMaximum(1000)
+        self.max_tokens_spin.setMaximum(9999)
         self.max_tokens_spin.setValue(100)
         self.max_tokens_spin.setEnabled(False)  # Disabled by default
         max_tokens_layout = QHBoxLayout()
@@ -49,37 +56,48 @@ class AppWindow(QMainWindow):
         temperature_layout.addWidget(self.temperature_slider)
         temperature_layout.addWidget(self.temperature_value_label)
 
+        # Ensure consistent height for controls
+        self.max_tokens_spin.setFixedHeight(25)
+        self.temperature_slider.setFixedHeight(25)
+        self.temperature_value_label.setFixedHeight(25)
+        self.temperature_checkbox.setFixedHeight(25)
+        self.max_tokens_checkbox.setFixedHeight(25)
+
         # Output label
         self.output_label = QLabel("Enter your question and press submit.", self)
         self.output_label.setWordWrap(True)  # Enable word wrap
 
         # Scroll Area for output label
         self.scroll_area = QScrollArea(self)
-        self.scroll_area.setWidgetResizable(True)  # Make the scroll area resizable
+        self.scroll_area.setWidgetResizable(True)
         self.scroll_area_widget_contents = QWidget()
         self.scroll_area.setWidget(self.scroll_area_widget_contents)
         layout_scroll_area = QVBoxLayout(self.scroll_area_widget_contents)
         layout_scroll_area.addWidget(self.output_label)
-        self.scroll_area.setMinimumHeight(100)  # Set a minimum height for the scroll area
-
+        
         # Add widgets to layout
         layout.addWidget(self.input_line)
         layout.addLayout(max_tokens_layout)
         layout.addLayout(temperature_layout)
-        layout.addWidget(self.submit_button)
-        # layout.addWidget(self.output_label)
-        layout.addWidget(self.scroll_area)
+        # layout.addWidget(self.submit_button)
+        # layout.addWidget(self.scroll_area)
 
+        # Create a horizontal layout for the buttons
+        buttons_layout = QHBoxLayout()
+        buttons_layout.addStretch()  # Add a stretchable space before the buttons (pushes them to center)
+        buttons_layout.addWidget(self.submit_button)  # Add submit button
+        buttons_layout.addWidget(self.save_button)  # Add save button
+        buttons_layout.addStretch()  # Add a stretchable space after the buttons (pushes them to center)
+       
+        # Add the buttons layout to the main layout
+        layout.addLayout(buttons_layout)
+        layout.addWidget(self.scroll_area)
 
         # Set the layout
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
-
-        # Add a Save to .docx button
-        self.save_button = QPushButton('Save to .docx', self)
-        self.save_button.clicked.connect(self.on_save_click)
-        layout.addWidget(self.save_button)
+        # layout.addWidget(self.save_button)
 
     def on_submit_click(self):
         user_input = self.input_line.text()
